e8b13564f33a9bce369f0e0dcfbbdb596e401246
Robert Springer <springerdrums@gmail.com>
2016-04-12 15:08:31 -0700

    logout button

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
 
 
   # logout
-  # delete '/logout' => 'users/destroy'
+  delete '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
